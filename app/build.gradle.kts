6f4db6a57242aa5bc01111dd580ffd181d3b7003
Sure Serverman <dupastiy@proton.me>
2026-05-02 14:59:50 +0100

    feat(release): update version to 1.1.0 and modify F-Droid metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.matrix.synapse.manager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "com.matrix.synapse.manager.HiltTestRunner"
     }
